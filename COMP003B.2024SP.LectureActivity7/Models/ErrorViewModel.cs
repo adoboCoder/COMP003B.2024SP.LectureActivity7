46ba9846e0442930339c27f193292f88133ba4c7
adobocoder <jcruz@adobo.dev>
2024-05-06 18:18:52 -0700

    Add project files.

---
+++ COMP003B.2024SP.LectureActivity7/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COMP003B._2024SP.LectureActivity7.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
